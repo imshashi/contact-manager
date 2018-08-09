0dda052c67b437535cd5fcabfaff11976172762c
Shashi Kashyap <shashi.aimit@hotmail.com>
2018-08-09 18:18:53 +0530

    add Context reducer and action, update delete feature with Context api

--- src/components/Contacts.js
+++ src/components/Contacts.js
@@ -4,13 +4,6 @@ import { Consumer } from '../context';
 
 class Contacts extends Component {
 
-  deleteContact = (id) => {
-    const { contacts } = this.state;
-    const newContacts = contacts.filter(contact => contact.id !== id);
-
-    this.setState({ contacts: newContacts });
-  }
-
   render() {
     return (
       <Consumer>
@@ -22,7 +15,6 @@ class Contacts extends Component {
                 <Contact
                   key={ contact.id }
                   contact={ contact }
-                  deleteClickHandler={ this.deleteContact.bind(this, contact.id) }
                 />
               )) }
             </Fragment>
