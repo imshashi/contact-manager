3f0982a9bbd3333886968ee164d4e18d650ee8df
Shashi Kashyap <shashi.aimit@hotmail.com>
2018-08-09 16:06:43 +0530

    toggle contact dropdown and allow delete contact

--- src/components/Contact.js
+++ src/components/Contact.js
@@ -2,22 +2,51 @@ import React, { Component } from 'react';
 import PropTypes from 'prop-types';
 
 class Contact extends Component {
+  state = {
+    showContactInfo: false
+  };
+
+  onShowClick = e => {
+    this.setState({ showContactInfo: !this.state.showContactInfo })
+  }
+
+  onDeleteClick = e => {
+    this.props.deleteClickHandler();
+  }
+
   render() {
     const { name, email, phone } = this.props.contact;
+    const { showContactInfo } = this.state;
 
     return (
       <div className="card card-body mb-3">
-        <h4>{ name }</h4>
-        <ul className="list-group">
-          <li className="list-group-item">Email: { email }</li>
-          <li className="list-group-item">Phone: { phone }</li>
-        </ul>
+        <h4>
+          { name }
+          <i
+            className="fas fa-caret-down"
+            onClick={ this.onShowClick }
+            style={{ cursor: 'pointer' }}
+          />
+          <i
+            className="fas fa-times"
+            style={{ cursor: 'pointer', float: 'right', color: 'red' }}
+            onClick={ this.onDeleteClick }
+          />
+        </h4>
+        { showContactInfo ? (
+          <ul className="list-group">
+            <li className="list-group-item">Email: { email }</li>
+            <li className="list-group-item">Phone: { phone }</li>
+          </ul>
+        ) : null }
+
       </div>
     )
   }
 }
 
 Contact.propTypes = {
+  deleteClickHandler: PropTypes.func.isRequired,
   contact: PropTypes.object.isRequired
 }
 
--- src/components/Contacts.js
+++ src/components/Contacts.js
@@ -1,4 +1,4 @@
-import React, { Component } from 'react';
+import React, { Component, Fragment } from 'react';
 import Contact from './Contact';
 
 class Contacts extends Component {
@@ -28,18 +28,26 @@ class Contacts extends Component {
     }
   }
 
+  deleteContact = (id) => {
+    const { contacts } = this.state;
+    const newContacts = contacts.filter(contact => contact.id !== id);
+
+    this.setState({ contacts: newContacts });
+  }
+
   render() {
     const { contacts } = this.state;
 
     return (
-      <div>
+      <Fragment>
         { contacts.map(contact => (
           <Contact
             key={ contact.id }
             contact={ contact }
+            deleteClickHandler={ this.deleteContact.bind(this, contact.id) }
           />
         )) }
-      </div>
+      </Fragment>
     );
   }
 }
