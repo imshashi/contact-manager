2df2448e1f891da479605fd229102e507ce174a7
Shashi Kashyap <shashi.aimit@hotmail.com>
2018-08-09 13:26:31 +0530

    add Contact component with basic layout

--- src/App.js
+++ src/App.js
@@ -1,11 +1,18 @@
 import React, { Component } from 'react';
-import './App.css';
+import Contact from './components/Contact';
+import Header from './components/Header';
+
+import 'bootstrap/dist/css/bootstrap.min.css';
 
 class App extends Component {
   render() {
     return (
       <div className="App">
-        <h1>The React App</h1>
+        <Header branding="Contact Manager" />
+        <div className="container">
+          <Contact name="Shashi Kashyap" email="shashi@gmail.com" phone="555-555-5555" />
+          <Contact name="Ravi Singh" email="ravi@gmail.com" phone="333-333-3333" />
+        </div>
       </div>
     );
   }
