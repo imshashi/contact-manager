326bf5170987c0f7a1bb12ce6a8e1f95b2132201
Shashi Kashyap <shashi.aimit@hotmail.com>
2018-08-11 01:23:11 +0530

    React router setup

--- src/App.js
+++ src/App.js
@@ -1,6 +1,9 @@
 import React, { Component } from 'react';
+import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
+
 import Contacts from './components/contacts/Contacts';
 import AddContact from './components/contacts/AddContact';
+import About from './components/pages/About';
 import Header from './components/layout/Header';
 
 import { Provider } from './context';
@@ -11,13 +14,18 @@ class App extends Component {
   render() {
     return (
       <Provider>
-        <div className="App">
-          <Header branding="Contact Manager" />
-          <div className="container">
-            <AddContact />
-            <Contacts />
+        <Router>
+          <div className="App">
+            <Header branding="Contact Manager" />
+            <div className="container">
+              <Switch>
+                <Route exact path="/" component={ Contacts } />
+                <Route exact path="/contacts/new" component={ AddContact } />
+                <Route exact path="/about" component={ About } />
+              </Switch>
+            </div>
           </div>
-        </div>
+        </Router>
       </Provider>
     );
   }
--- src/components/layout/Header.js
+++ src/components/layout/Header.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import PropTypes from 'prop-types';
+import { Link } from 'react-router-dom';
 
 const Header = (props) => {
   const { branding } = props;
@@ -7,11 +8,23 @@ const Header = (props) => {
   return (
     <nav className="navbar navbar-expand-sm navbar-dark bg-danger mb-3 py-0">
       <div className="container">
-        <a href="/" className="navbar-brand">{ branding }</a>
+        <Link to="/" className="navbar-brand">{ branding }</Link>
         <div className="">
           <ul className="navbar-nav mr-auto">
             <li className="nav-item">
-              <a href="/" className="nav-link">Home</a>
+              <Link to="/" className="nav-link">
+                <i className="fas fa-home" /> Home
+              </Link>
+            </li>
+            <li className="nav-item">
+              <Link to="/contacts/new" className="nav-link">
+                <i className="fas fa-plus" /> Add Contact
+              </Link>
+            </li>
+            <li className="nav-item">
+              <Link to="/about" className="nav-link">
+                <i className="fas fa-question" />  About
+              </Link>
             </li>
           </ul>
         </div>
