19640057a807cd3be481bb42ec4f908d46993083
Shashi Kashyap <shashi.aimit@hotmail.com>
2018-08-12 16:50:22 +0530

    create EditContact component

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
 
 import Contacts from './components/contacts/Contacts';
 import AddContact from './components/contacts/AddContact';
+import EditContact from './components/contacts/EditContact';
 import About from './components/pages/About';
 import NotFound from './components/pages/NotFound';
 import Header from './components/layout/Header';
@@ -22,7 +23,9 @@ class App extends Component {
               <Switch>
                 <Route exact path="/" component={ Contacts } />
                 <Route exact path="/contacts/new" component={ AddContact } />
+                <Route exact path="/contacts/:id/edit" component={ EditContact } />
                 <Route exact path="/about" component={ About } />
+                <Route exact path="/test" component={ Test } />
                 <Route exact path="/not-found" component={ NotFound } />
               </Switch>
             </div>
